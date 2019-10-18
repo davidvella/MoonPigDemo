bd215c56e4d51178725f928fbc0c67f4b01368c2
David Vella <david@vella.uk.com>
2019-10-18 01:23:26 +0100

    Upload Page

---
+++ precache-manifest.45703bcffd4de91019ae256a1ab173bd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f708212eea699b9dd2aeba10d6180868",
+    "url": "/MoonPigDemo/index.html"
+  },
+  {
+    "revision": "08f14b1ae97087a8e48f",
+    "url": "/MoonPigDemo/static/css/2.991579c9.chunk.css"
+  },
+  {
+    "revision": "c3fc7b10d2563c1a38cf",
+    "url": "/MoonPigDemo/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "08f14b1ae97087a8e48f",
+    "url": "/MoonPigDemo/static/js/2.c464335c.chunk.js"
+  },
+  {
+    "revision": "c3fc7b10d2563c1a38cf",
+    "url": "/MoonPigDemo/static/js/main.36d4c553.chunk.js"
+  },
+  {
+    "revision": "0fb4da7da01e614d5886",
+    "url": "/MoonPigDemo/static/js/runtime-main.d86283b9.js"
+  }
+]);
